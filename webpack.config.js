e8e81ad39527621495492f1f531e13025e787fe9
guipure <igor778868@gmail.com>
2020-04-03 21:38:39 +0300

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const path = require('path')
+const HTMLWebpackPlugin = require('html-webpack-plugin')
+const {CleanWebpackPlugin} = require('clean-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+
+module.exports = {
+    mode: 'development',
+    entry: './src/js/index.js',
+    output: {
+        filename: '[name].js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.pug$/,
+                loader: 'pug-loader',
+                options: {
+                    pretty: true
+                }
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    'sass-loader'
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new HTMLWebpackPlugin({
+            filename: 'index.html',
+            template: './src/pug/index.pug'
+        }),
+        new CleanWebpackPlugin(),
+        new MiniCssExtractPlugin({
+            filename: '[name].css'
+        })
+    ]
+}
