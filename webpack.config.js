3f1add5a56fbcf7073a0cd8f864949bc510367f2
guipure <igor778868@gmail.com>
2020-04-04 00:20:33 +0300

    File structure reorganization

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 module.exports = {
     mode: 'development',
-    entry: './src/js/index.js',
+    entry: './src/index.js',
     output: {
         filename: '[name].js',
         path: path.resolve(__dirname, 'dist')
@@ -32,7 +32,7 @@ module.exports = {
     plugins: [
         new HTMLWebpackPlugin({
             filename: 'index.html',
-            template: './src/pug/index.pug'
+            template: './src/index.pug'
         }),
         new CleanWebpackPlugin(),
         new MiniCssExtractPlugin({
