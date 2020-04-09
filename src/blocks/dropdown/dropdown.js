9a2a15c2415306cfa7df08726c583f981bf08176
guipure <igor778868@gmail.com>
2020-04-09 23:55:24 +0300

    Added beds calculator

--- src/blocks/dropdown/dropdown.js
+++ src/blocks/dropdown/dropdown.js
@@ -2,6 +2,9 @@ const boxes = document.querySelectorAll('.dropdown__box');
 const pluses = document.querySelectorAll('.dropdown__operator_plus');
 const minuses = document.querySelectorAll('.dropdown__operator_minus');
 const clearBtns = document.querySelectorAll('.dropdown__clear');
+const submitBtns = document.querySelectorAll('.dropdown__submit');
+
+window.addEventListener('click', hide);
 
 for (let box of boxes) {
     box.addEventListener('click', show);
@@ -21,10 +24,39 @@ for (let btn of clearBtns) {
     btn.firstElementChild.addEventListener('click', () => clear(dropdown, result));
 }
 
-function show() {
-    const content = event.currentTarget.nextElementSibling;
-    content.classList.toggle('dropdown__content_hidden');
+for (let btn of submitBtns) {
+    btn.addEventListener('click', submit);
 }
+
+
+function submit() {
+    show();
+}
+
+
+function show() {
+    const dropdown = event.currentTarget.closest('.dropdown');
+    const menu = dropdown.querySelector('.dropdown__menu');
+    const icon = dropdown.querySelector('.dropdown__icon');
+    menu.classList.toggle('dropdown__menu_hidden');
+    icon.classList.toggle('dropdown__icon_rotated');
+}
+
+
+function hide() {
+    if (event.target.closest('.dropdown')) return;
+    const menus = document.querySelectorAll('.dropdown__menu');  
+    const rotatedIcons = document.querySelectorAll('.dropdown__icon_rotated');
+
+    for (let icon of rotatedIcons) {
+        icon.classList.remove('dropdown__icon_rotated');
+    }
+
+    for (let menu of menus) {
+        menu.classList.add('dropdown__menu_hidden');
+    } 
+}
+
 
 function operator(sign) {
     let valueDiv;
@@ -46,37 +78,71 @@ function operator(sign) {
         minusDiv.classList.remove('dropdown__operator_disabled');
     }
     valueDiv.innerHTML = value.toString();
-    guestsSum(event.target.closest('.dropdown__content'));
+    sum(event.target.closest('.dropdown'));
 }
 
 
-function guestsSum(content) {
-    const values = content.querySelectorAll('.dropdown__value');
-    const result = content.previousElementSibling.querySelector('.dropdown__result');
-    let sum = 0;
-    let ending;
+function sum(dropdown) {
+    const values = Array.from(dropdown.querySelectorAll('.dropdown__value')).map((value) => Number(value.innerHTML));
+    const result = dropdown.querySelector('.dropdown__result');
+    const labels = Array.from(dropdown.querySelectorAll('.dropdown__label')).map(label => label.innerHTML);
+    if (result.classList.contains('all')) {
+        let sum = values.reduce((a, b) => a + b, 0)
+        result.innerHTML = sum.toString() + ending(sum, 'гость');
+    } else {
+        let itemsCount = [];
 
-    for (let v of values) {
-        sum += Number(v.innerHTML);
+        values.forEach((value, index) => {
+            if (value > 0) {
+                itemsCount.push(value.toString() + ending(value, labels[index]));
+            }
+        });
+
+        if (itemsCount.length > 2) {
+            result.innerHTML = itemsCount.slice(0, 2).join(', ') + '...';
+        } else {
+            result.innerHTML = itemsCount.join(', ');
+        }
+    }
+}
+
+
+function ending(value, word) {
+    let types;
+
+    switch (word) {
+        case 'спальни':
+            types = ['спальня', 'спальни', 'спален'];
+            break;
+        case 'кровати':
+            types = ['кровать', 'кровати', 'кроватей'];
+            break;
+        case 'ванные комнаты':
+            types = ['ванная', 'ванных', 'ванных'];
+            break;
+        case 'гость':
+            types = ['гость', 'гостя', 'гостей'];
+            break;
     }
 
-    if (sum > 4 && sum < 21) {
-        ending = ' гостей';
-    } else if (sum % 10 == 1) {
-        ending = ' гость';
-    } else if (sum % 10 > 1 && sum % 10 < 5) {
-        ending = ' гостя';
-    } else ending = ' гостей';
-
-    result.innerHTML = sum.toString() + ending;
+    if (value > 4 && value < 21) return (' ' + types[2]);
+    else if (value % 10 == 1) return (' ' + types[0]);
+    else if (value % 10 > 1 && value % 10 < 5) return (' ' + types[1]);
+    else return (' ' + types[2]);
 }
 
 
 function clear(dropdown, result) {
-    const values = [...dropdown.querySelectorAll('.dropdown__value')];
-    values.map((value) => {
+    const values = dropdown.querySelectorAll('.dropdown__value');
+
+    for (let value of values) {
         value.innerHTML = '0';
         value.previousElementSibling.classList.add('dropdown__operator_disabled');
-    });
-    result.innerHTML = 'Сколько гостей';
+    }
+
+    if (result.classList.contains('all')) {
+        result.innerHTML = 'Сколько гостей';
+    } else {
+        result.innerHTML = 'Сколько кроватей';
+    }
 }
