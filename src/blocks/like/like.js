bb8df5da820090e1d301ceae471446be22b3c03d
guipure <igor778868@gmail.com>
2020-04-19 23:30:27 +0300

    Added like button block

---
+++ src/blocks/like/like.js
@@ -0,0 +1,24 @@
+const likes = document.querySelectorAll('.js-like');
+likes.forEach(like => like.addEventListener('click', (e) => likeToggle(e)))
+
+
+function likeToggle(event) {
+    const likeDiv = event.currentTarget;
+    const iconSpan = likeDiv.querySelector('.js-like__icon');
+    const numberSpan = likeDiv.querySelector('.js-like__number');
+    likeDiv.classList.toggle('js-like_liked');
+    iconToggle(iconSpan);
+    numberToggle(numberSpan);
+}
+
+
+function iconToggle(iconSpan) {
+    iconSpan.innerHTML = iconSpan.innerHTML == 'favorite_border' ? 'favorite' : 'favorite_border';
+}
+
+
+function numberToggle(numberSpan) {
+    const number = Number(numberSpan.innerHTML);
+    const isLiked = numberSpan.closest('.js-like_liked') ? true : false;
+    numberSpan.innerHTML = isLiked ? (number + 1).toString() : (number - 1).toString();
+}
