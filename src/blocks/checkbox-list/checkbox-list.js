21d870a58e5ee924e06a6d59b8c0454a863c975d
guipure <igor778868@gmail.com>
2020-04-23 21:48:03 +0300

    Added expandable checkbox list block

---
+++ src/blocks/checkbox-list/checkbox-list.js
@@ -0,0 +1,20 @@
+class CheckboxList {
+  constructor(element) {
+    const dropdown = element.children('.js-checkbox-list__dropdown');
+    const label = element.children('.js-checkbox-list__label');
+    label.click(() => this.slide(dropdown, label));
+  }
+
+
+  slide(dropdown, label) {
+    const icon = label.children('.js-checkbox-list__icon');
+    icon.toggleClass('js-checkbox-list__icon_rotated')
+    if (dropdown.is(':hidden')) {
+      dropdown.slideDown('fast');
+    } else dropdown.hide('fast');
+  }
+}
+
+
+const lists = $(".js-checkbox-list");
+lists.each((index, element) => new CheckboxList($(element)));
