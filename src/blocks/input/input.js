e436f7cc92a40ce0c3274d44bcd82e9ed0b77ac0
guipure <igor778868@gmail.com>
2020-07-27 21:01:29 +0300

    Added input types

--- src/blocks/input/input.js
+++ src/blocks/input/input.js
@@ -7,15 +7,18 @@ function checkKey(event) {
   const regDay = /^\d\d$/;
   const regDayMonth = /^\d\d\.\d\d$/;
   const regDayMonthYear = /^\d{2}\.\d{2}\.\d{4}$/;
-  if ((key >= '0' && key <= '9') && !regDayMonthYear.test(value)) {
+  const keyIsDigit = key >= '0' && key <= '9';
+  const keyIsControl = (
+    key === 'ArrowLeft'
+    || key === 'ArrowRight'
+    || key === 'Delete'
+    || key === 'Backspace'
+  );
+  if (keyIsDigit && !regDayMonthYear.test(value)) {
     if (regDay.test(value + key) || regDayMonth.test(value + key)) {
       event.target.value += `${key}.`;
       event.preventDefault();
     }
-  } else if (key == 'ArrowLeft'
-    || key == 'ArrowRight'
-    || key == 'Delete'
-    || key == 'Backspace') {
-
+  } else if (keyIsControl) {
   } else event.preventDefault();
 }
