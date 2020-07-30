5ad6d7944ef7106f24ec92dc4bd83a331353cc99
guipure <igor778868@gmail.com>
2020-07-30 20:00:48 +0300

    Checkbox refactoring

--- src/blocks/checkbox-list/checkbox-list.js
+++ src/blocks/checkbox-list/checkbox-list.js
@@ -1,18 +1,18 @@
 class CheckboxList {
-  constructor(element) {
-    const dropdown = element.children('.js-checkbox-list__dropdown');
-    const label = element.children('.js-checkbox-list__label');
-    label.click(() => this.slide(dropdown, label));
+  constructor($element) {
+    const $dropdown = $element.find('.js-checkbox-list__dropdown');
+    const $label = $element.children('.js-checkbox-list__label');
+    $label.on('click', this.slide.bind(this, $dropdown, $label));
   }
 
-  slide(dropdown, label) {
-    const icon = label.children('.js-checkbox-list__icon');
-    icon.toggleClass('js-checkbox-list__icon_rotated');
-    if (dropdown.is(':hidden')) {
-      dropdown.slideDown('fast');
-    } else dropdown.hide('fast');
+  slide($dropdown, $label) {
+    const $icon = $label.find('.js-checkbox-list__icon');
+    $icon.toggleClass('js-checkbox-list__icon_rotated');
+    if ($dropdown.is(':hidden')) {
+      $dropdown.slideDown('fast');
+    } else $dropdown.hide('fast');
   }
 }
 
-const lists = $('.js-checkbox-list');
-lists.each((index, element) => new CheckboxList($(element)));
+const $lists = $('.js-checkbox-list');
+$lists.each((index, element) => new CheckboxList($(element)));
