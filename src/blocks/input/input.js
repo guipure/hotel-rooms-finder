be8afecf0509ee9f424922e77847d6f639c50935
guipure <igor778868@gmail.com>
2020-08-03 11:52:53 +0300

    Input refactoring

--- src/blocks/input/input.js
+++ src/blocks/input/input.js
@@ -1,24 +1,7 @@
-const inputs = document.querySelectorAll('.input__box_masked');
-Array.from(inputs).forEach((input) => input.addEventListener('keydown', (event) => checkKey(event)));
+import 'jquery.maskedinput/src/jquery.maskedinput';
 
-function checkKey(event) {
-  const { key } = event;
-  const { value } = event.target;
-  const regDay = /^\d\d$/;
-  const regDayMonth = /^\d\d\.\d\d$/;
-  const regDayMonthYear = /^\d{2}\.\d{2}\.\d{4}$/;
-  const keyIsDigit = key >= '0' && key <= '9';
-  const keyIsControl = (
-    key === 'ArrowLeft'
-    || key === 'ArrowRight'
-    || key === 'Delete'
-    || key === 'Backspace'
-  );
-  if (keyIsDigit && !regDayMonthYear.test(value)) {
-    if (regDay.test(value + key) || regDayMonth.test(value + key)) {
-      event.target.value += `${key}.`;
-      event.preventDefault();
-    }
-  } else if (keyIsControl) {
-  } else event.preventDefault();
-}
+$.mask.definitions.y = '[12]';
+$.mask.definitions.m = '[01]';
+$.mask.definitions.d = '[0-3]';
+
+$('.js-input__box_masked').each((index, element) => $(element).mask('d9.m9.y999'));
